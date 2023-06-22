0aa36c4e4ba1a19a72f89098b3ae38efd3770df6
AlanBorjes <83984786+AlanBorjes@users.noreply.github.com>
2023-06-21 19:45:56 -0600

    $ git commit -m

--- Sheep.cs
+++ Sheep.cs
@@ -1,9 +1,10 @@
-using System;
+sing System;
 
 class Sheep : IAnimal
 {
     public void MakeSound()
     {
         Console.WriteLine("English: Baa! Baa! Baa!");
+        Console.WriteLine("Spanish: Beee! Beee! Beee!");
     }
 }
